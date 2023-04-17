4df0c5cedf8b9fe6cb0ceb6a303b2123d7845772
Yasin <122858551+Yasinvalipoor@users.noreply.github.com>
2023-04-17 21:48:33 +0330

    Update Prediction.cs

--- Predictor/Prediction.cs
+++ Predictor/Prediction.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ConsoleTables;
 
 
@@ -20,20 +15,12 @@ namespace Cryptocurrency_analysis.Predictor
 
 
         //متد پیش بینی2   
-        public double AveragePrediction2(List<double> Information)
-        {
-            return Information.Average();
-        }
-
-
-
-        //متد پیش بینی3   
-        public double RandomPrediction3(double RandomPrediction1, double AveragePrediction2)
+        public double Random_Range(double RandomPrediction1, double AveragePrediction2)
         {
             Random random = new Random();
-
-            int pre1 = Convert.ToInt32(RandomPrediction1);
-            int pre2 = Convert.ToInt32(AveragePrediction2);
+            List<int> avarage = new List<int>();
+            int pre1 = (int)RandomPrediction1;
+            int pre2 = (int)AveragePrediction2;
             if (pre1 > pre2)
             {
                 int new_rnd = random.Next(pre2, pre1);
@@ -44,27 +31,21 @@ namespace Cryptocurrency_analysis.Predictor
             {
                 int new_rnd = random.Next(pre1, pre2);
                 return new_rnd;
-
             }
-
-
-
-
         }
 
-
         //متد صحت سنجی پیش بینی ها
-        public void ValidatePrediction(double Information_Price, double RandomPrediction1, double AveragePrediction2, double RandomPrediction3, double smart_pre, double SmartAvarage)
+        public void ValidatePredictions(double Information_Price, double RandomPrediction1, double RandomPrediction2, double AveragePrediction3, double smart_reg)
         {
 
             double Price = Information_Price;
             double pre1 = RandomPrediction1;
-            double pre2 = AveragePrediction2;
-            double pre3 = RandomPrediction3;
-            double pre4 = smart_pre;
-            double pre5 = SmartAvarage;
+            double pre2 = RandomPrediction2;
+            double pre3 = AveragePrediction3;
+            double pre4 = smart_reg;
 
-            List<double> LPrice = new List<double>();
+            List<double> Pos_Price = new List<double>();//نرخ های مثبت
+            List<double> Neg_Price = new List<double>();//نرح های منفی
             var table = new ConsoleTable("                 << Details Of Accuracy Differences And Percentage Difference >>                 ");
 
             /////////
@@ -74,13 +55,14 @@ namespace Cryptocurrency_analysis.Predictor
                 var Price1 = Price - pre1;
                 var Percentage_Difference = (Price1 * 100) / Price;
                 table.AddRow($"1) Price - Random1 :  {Price1}  ___  Percentage Difference  : - {Percentage_Difference} % decrease");
-                LPrice.Add(Price1);
+                Pos_Price.Add(Price1);
             }
             else
             {
                 var Price1 = Price - pre1;
                 var Percentage_Difference = ((pre1 - Price) * 100) / Price;
                 table.AddRow($"1) Price - Random1 :  {Price1}  ___  Percentage Difference  :  {Percentage_Difference} % increase");
+                Neg_Price.Add(Price1);
 
             }
 
@@ -88,79 +70,68 @@ namespace Cryptocurrency_analysis.Predictor
 
             if (Price - pre2 > 0)
             {
+
                 var Price2 = Price - pre2;
                 var Percentage_Difference = (Price2 * 100) / Price;
-                table.AddRow($"2) Price - Average :  {Price2}  ___  Percentage Difference  : - {Percentage_Difference} % decrease");
-                LPrice.Add(Price2);
+                table.AddRow($"2) Price - Random2 :  {Price2}  ___  Percentage Difference  : - {Percentage_Difference} % decrease");
+                Pos_Price.Add(Price2);
+
             }
             else
             {
                 var Price2 = Price - pre2;
                 var Percentage_Difference = ((pre2 - Price) * 100) / Price;
-                table.AddRow($"2) Price - Average :  {Price2}  ___  Percentage Difference  :  {Percentage_Difference} % increase");
+                table.AddRow($"2) Price - Random2 :  {Price2}  ___  Percentage Difference  :  {Percentage_Difference} % increase");
+                Neg_Price.Add(Price2);
 
             }
 
             /////////
 
-            if (Price - pre3 > 0)
+            if (Price - pre3 >= 0)
             {
                 var Price3 = Price - pre3;
                 var Percentage_Difference = (Price3 * 100) / Price;
-                table.AddRow($"3) Price - Random2 :  {Price3}  ___  Percentage Difference  : - {Percentage_Difference} % decrease");
-                LPrice.Add(Price3);
+                table.AddRow($"3) Price - Average :  {Price3}  ___  Percentage Difference  : - {Percentage_Difference} % decrease");
+                Pos_Price.Add(Price3);
             }
             else
             {
                 var Price3 = Price - pre3;
                 var Percentage_Difference = ((pre3 - Price) * 100) / Price;
-                table.AddRow($"3) Price - Random2 :  {Price3}  ___  Percentage Difference  :  {Percentage_Difference} % increase");
+                table.AddRow($"3) Price - Average :  {Price3}  ___  Percentage Difference  :  {Percentage_Difference} % increase");
+                Neg_Price.Add(Price3);
 
             }
 
             /////////
 
-            if (Price - pre4 > 0)
+            if (Price - pre4 >= 0)
             {
                 var Price4 = Price - pre4;
                 var Percentage_Difference = (Price4 * 100) / Price;
                 table.AddRow($"4) Price - Regression :  {Price4}  ___  Percentage Difference  : - {Percentage_Difference} % decrease");
-                LPrice.Add(Price4);
+                Pos_Price.Add(Price4);
             }
             else
             {
                 var Price4 = Price - pre4;
-                var Percentage_Difference = ((pre4 - Price) * 100) / Price;
+                var Percentage_Difference = ((pre4 - Price) * 100) / Price4;
                 table.AddRow($"4) Price - Regression :  {Price4}  ___  Percentage Difference  :  {Percentage_Difference} % increase");
+                Neg_Price.Add(Price4);
 
             }
 
-            ////////////
-
-            if (Price - pre5 > 0)
-            {
-                var Price5 = Price - pre5;
-                var Percentage_Difference = (Price5 * 100) / Price;
-                table.AddRow($"5) Price - SmartAvarage :  {Price5}  ___  Percentage Difference  : - {Percentage_Difference} % decrease");
-                LPrice.Add(Price5);
-            }
-            else
-            {
-                var Price5 = Price - pre5;
-                var Percentage_Difference = ((pre5 - Price) * 100) / Price;
-                table.AddRow($"5) Price - SmartAvarage :  {Price5}  ___  Percentage Difference  :  {Percentage_Difference} % increase");
-
-            }
 
             ////////////
 
             Console.WriteLine(table.ToStringAlternative());
 
 
-            if (LPrice.Count > 0)
+            if (Pos_Price.Count > 0)
             {
-                double min = LPrice.Min();
-                double max = LPrice.Max();
+                double Pmin = Pos_Price.Min();
+                double Pmax = Pos_Price.Max();
 
                 ////////
 
@@ -171,43 +142,69 @@ namespace Cryptocurrency_analysis.Predictor
                 ///////
 
 
-                if (min + pre1 == Price)
+                if (Pmin + pre1 == Price)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($" The Smallest difference Is : {min} And It's For \"First Prediction\" ");
+                    Console.WriteLine($" The Smallest in Positive difference Is : {Pmin} And It's For \"First Prediction\" ");
                     Console.ResetColor();
                 }
-                if (min + pre2 == Price)
+                if (Pmin + pre2 == Price)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($" The Smallest difference Is : {min} And It's For \"Second Prediction\" ");
+                    Console.WriteLine($" The Smallest in Positive difference Is : {Pmin} And It's For \"Second Prediction\" ");
                     Console.ResetColor();
                 }
-                if (min + pre3 == Price)
+                if (Pmin + pre3 == Price)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($" The Smallest difference Is : {min} And It's For \"Third Prediction\" ");
+                    Console.WriteLine($" The Smallest in Positive difference Is : {Pmin} And It's For \"Rng_Prediction\" ");
                     Console.ResetColor();
                 }
-                if (min + pre4 == Price)
+                if (Pmin + pre4 == Price)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($" The Smallest difference Is : {min} And It's For \"Liner_Regression\" ");
-                    Console.ResetColor();
-                }
-                if (min + pre5 == Price)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($" The Smallest difference Is : {min} And It's For \"Smart_Avarage\" ");
+                    Console.WriteLine($" The Smallest in Positive difference Is : {Pmin} And It's For \"Liner_Regression\" ");
                     Console.ResetColor();
                 }
 
-                ////////
 
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($" The Biggest difference Is : {max}\n");
-                Console.ResetColor();
 
+                if (Neg_Price.Count > 0)
+                {
+                    double Nmin = Neg_Price.Max();
+
+                    if (Nmin + pre1 == Price)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($" The Smallest in Negative difference Is : {Nmin} And It's For \"First Prediction\" ");
+                        Console.ResetColor();
+                    }
+                    if (Nmin + pre2 == Price)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($" The Smallest in Negative difference Is : {Nmin} And It's For \"Second Prediction\" ");
+                        Console.ResetColor();
+                    }
+                    if (Nmin + pre3 == Price)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($" The Smalles in Negativet difference Is : {Nmin} And It's For \"Rng_Prediction\" ");
+                        Console.ResetColor();
+                    }
+                    if (Nmin + pre4 == Price)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($" The Smallest in Negative difference Is : {Nmin} And It's For \"Liner_Regression\" ");
+                        Console.ResetColor();
+                    }
+
+                    ////////
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($" The Biggest difference Is : {Pmax}\n");
+                    Console.ResetColor();
+
+                }
             }
         }
     }
